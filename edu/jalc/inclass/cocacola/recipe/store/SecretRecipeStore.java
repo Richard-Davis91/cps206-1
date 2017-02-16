8e278f11d1389aa42d6509dc5a00edf86b5b1dad
Richard-Davis91 <mogiz91@gmail.com>
2017-02-16 12:42:13 -0600

    changes

--- edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStore.java
+++ edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStore.java
@@ -1,9 +1,9 @@
 package edu.jalc.inclass.cocacola.recipe.store;
-
-
+import edu.jalc.inclass.cocacola.recipe.Recipe;
+import edu.jalc.inclass.cocacola.security.PinNumber;
+import edu.jalc.inclass.cocacola.employee.Employee;
 
 /*
-
 		This is a store of secret recipes.
 
 		There can only be one instance of the store!
@@ -12,5 +12,17 @@ package edu.jalc.inclass.cocacola.recipe.store;
 		Make sure your `getRecipe` method checks for the employee's pin against the recipes pin! If the pins do not
 		match, throw an exception.
  */
+import java.util.HashMap;
+
 public class SecretRecipeStore {
+
+  private static SecretRecipeStore secretRecipeStore;
+  HashMap<Recipe,PinNumber> secretRecipe;
+
+  private SecretRecipeStore(){}
+
+  public static SecretRecipeStore getInstance(){
+    if(secretRecipeStore == null){secretRecipeStore = new SecretRecipeStore();}
+    return secretRecipeStore;
+  }
 }
--- edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStoreTest.java
+++ edu/jalc/inclass/cocacola/recipe/store/SecretRecipeStoreTest.java
@@ -1,4 +1,7 @@
 package edu.jalc.inclass.cocacola.recipe.store;
 
 public class SecretRecipeStoreTest {
+  public static void main(String... args){
+
+  }
 }
